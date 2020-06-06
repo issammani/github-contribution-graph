7675d1ae7adc71a8eb52bf63e68ec91130d72fb0
issammani <issamouu69@gmail.com>
2020-06-06 17:18:35 +0000

    Add contributions rendering
    - Add loop to generate cells column-wise and populate cells with count,
      date and class to assign color.
    
    - Add css classes to style cells.

--- src/graph.js
+++ src/graph.js
@@ -1,5 +1,7 @@
 import Svg from './svg.js';
 import Cell from './cell.js';
+import {getDayShortName, getMonthShortName} from './date.js';
+import generateMockContributions from './contribution.js';
 
 // Represents the contribution graph
 export default class Graph{
@@ -7,7 +9,6 @@ export default class Graph{
         
         this.containerSelector = props.containerSelector || 'body';
         this.cellCize = props.cellSize || 10;
-
         this.gap = props.gap || 10;
 
         this.DOMELement = this.createElement();
@@ -21,19 +22,32 @@ export default class Graph{
         // Get number of possible cells
         const _numberOfCells = Math.floor(_width / (this.cellCize + this.gap));
 
-        // Set width to fit graph
-        _width = _numberOfCells * (this.cellCize + this.gap);
+        // Set width to fit graph (+2 to account for the weekday column)
+        _width = (_numberOfCells + 2)  * (this.cellCize + this.gap);
 
         // Create a new svg
         const _svg = new Svg({parentSelector: this.containerSelector, width: _width, height: _height, class: 'graph__svg'});
-        
-       
-        
+
+        let i = 0, j = 0;
+        let contributionCountMax = 100;
+
         // Create cells
-        for(let i = 0; i < 7; i++){
-            for(let j = 0; j < _numberOfCells; j++){
-                new Cell({svgContainer: _svg, x: j * (this.cellCize + this.gap) , y: i * (this.cellCize + this.gap), date: '2020-02-03', count: 5, class: 'graph__cell'})
+        for(let contribution of generateMockContributions(7* _numberOfCells, contributionCountMax, 4)){
+            // Render cells column-wise
+            new Cell({
+                svgContainer: _svg, 
+                x: (i + 2) * (this.cellCize + this.gap),  // +2 to account for weekday text
+                y: j * (this.cellCize + this.gap), 
+                date: contribution.day.toLocaleDateString('en-GB'), 
+                count: contribution.count, 
+                class: `graph__cell contribution__level__${contribution.chunkIndex}`
+            });
+
+            if(j != 0 && (j+1) % 7 === 0 ){
+                i++;
             }
+            
+            j = (j + 1) % 7;  
         }
 
         return _svg;
